2e809472db9b53d9d6c7defdd3ec66f4d703f663
asif6070 <asifhossain6070@gmail.com>
2026-07-13 13:47:32 +0600

    Create Home component for Halal Mart Bangladesh

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{padding: "30px", textAlign: "center"}}>
+      <h1 style={{color: "green"}}>Halal Mart Bangladesh</h1>
+      <p>100% হাল প্রোডাক্ট</p>
+      <h2>আমাদের প্রোডাক্ট আসছে...</h2>
+    </main>
+  )
+}
